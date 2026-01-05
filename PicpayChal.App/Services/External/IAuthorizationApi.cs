abd092cb5193733f38dd67a98c867b11b030fa32
João Lima <jplima1853@gmail.com>
2026-01-05 10:33:25 -0300

    Adicionando consumo de serviço de autorização externo

---
+++ PicpayChal.App/Services/External/IAuthorizationApi.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+
+namespace PicpayChal.App.Services.External;
+
+public interface IAuthorizationApi
+{
+    [Get("/v2/authorize")]
+    Task<ApiResponse<AuthData>> AuthorizeTransfer();
+}
