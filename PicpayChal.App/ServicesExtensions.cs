89769b370f92e4618ae2bbc8612a70bd3565711b
João Lima <jplima1853@gmail.com>
2026-01-04 22:16:46 -0300

    extensão para configuração do banco de dados da aplicação

---
+++ PicpayChal.App/ServicesExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicpayChal.App.Data;
+
+namespace PicpayChal.App;
+
+public static class ServicesExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        var conn = configuration.GetConnectionString("sql_conn");
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(conn));
+    }
+}
