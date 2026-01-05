6b517185f662e9b45b72a4b195e390d41de7ffb3
João Lima <jplima1853@gmail.com>
2026-01-04 22:03:11 -0300

    Mapper para descrever entidade Wallet para o banco de dado com EF Core

---
+++ PicpayChal.App/Data/Maps/WalletMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Data.Maps;
+
+public sealed class WalletMap : IEntityTypeConfiguration<Wallet>
+{
+    public void Configure(EntityTypeBuilder<Wallet> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+        builder.HasIndex(x => x.Email).IsUnique();
+        builder.Property(x => x.Email).IsRequired();
+
+        builder.HasIndex(x => x.Cpf).IsUnique();
+        builder.Property(x => x.Cpf).IsRequired();
+
+        builder.Property(x => x.Balance).IsRequired();
+    }
+}
