401ca0e5be062d9ca8fb0c6c0229dd4c3bffce20
João Lima <jplima1853@gmail.com>
2026-01-05 08:20:06 -0300

    DTO para requisição de transferências

---
+++ PicpayChal.App/DTO/TransferRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PicpayChal.App.DTO;
+
+public record class TransferRequest
+{
+    [Required]
+    [JsonPropertyName("value")]
+    public decimal Value { get; set; }
+
+    [Required]
+    [JsonPropertyName("payer")]
+    public long PayerId { get; set; }
+
+    [Required]
+    [JsonPropertyName("payee")]
+    public long PayeeId { get; set; }
+}
