558db65b8be07a1835a985b7739712819fc1c0a7
João Lima <jplima1853@gmail.com>
2026-01-05 07:31:59 -0300

    repositório para Wallet

---
+++ PicpayChal.App/Repositories/WalletRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicpayChal.App.Data;
+using PicpayChal.App.Entities;
+using PicpayChal.App.Repositories.Interfaces;
+
+namespace PicpayChal.App.Repositories;
+
+public sealed class WalletRepository(AppDbContext context)
+        : BaseRepository<Wallet>(context)
+{
+    
+}
