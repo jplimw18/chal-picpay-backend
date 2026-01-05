f8a138f0b884c1cb0fa3e8c421821a4a673da322
João Lima <jplima1853@gmail.com>
2026-01-04 21:38:10 -0300

    Entidade para descrever transações

---
+++ PicpayChal.App/Entities/Transaction.cs
@@ -0,0 +1,23 @@
+namespace PicpayChal.App.Entities;
+
+public record Transaction(Wallet Payer, Wallet Payee, decimal Value)
+{
+    public long Id { get; private set; } = 0L;
+
+    public long PayerId { get; private set; } = 0L;
+    public long PayeeId { get; private set; } = 0L;
+
+    private Transaction()
+        : this(null!, null!, 0.0m) { }
+
+}
+
+
+public static class TransactionExtensions
+{
+    extension(Transaction)
+    {
+        public static Transaction Create(Wallet payer, Wallet payee, decimal value)
+            => new(payer, payee, value);
+    }
+}
