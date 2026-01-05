fbcc7227e88f9ce16566553967af03cfb5d93c06
João Lima <jplima1853@gmail.com>
2026-01-05 07:31:31 -0300

    Repositório base para operações com entidade base

---
+++ PicpayChal.App/Repositories/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicpayChal.App.Data;
+using PicpayChal.App.Entities;
+using PicpayChal.App.Repositories.Interfaces;
+
+namespace PicpayChal.App.Repositories;
+
+public abstract class BaseRepository<T>(AppDbContext context)
+    : IBaseRepository<T> where T : BaseEntity
+{
+
+    protected AppDbContext Context = context;
+
+    public T Create(T entity) =>
+        Context.Add(entity).Entity;
+
+    public void Delete(T entity) =>
+        Context.Remove(entity);
+
+    public Task<List<T>> GetAll() =>
+        Context.Set<T>().ToListAsync();
+
+    public Task<T> GetById(long id) =>
+        Context.Set<T>().FirstOrDefaultAsync(x => x.Id == id)!;
+
+    public T Update(T entity) =>
+        Context.Update(entity).Entity;
+}
