4596136db7a11b8be4da9cfe8a0fcc61e89dd659
João Lima <jplima1853@gmail.com>
2026-01-05 07:32:12 -0300

    Repositório para Transaction

---
+++ PicpayChal.App/Repositories/TransactionRepository.cs
@@ -0,0 +1,10 @@
+﻿using PicpayChal.App.Data;
+using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Repositories;
+
+public sealed class TransactionRepository(AppDbContext context)
+    : BaseRepository<Transaction>(context)
+{
+
+}
