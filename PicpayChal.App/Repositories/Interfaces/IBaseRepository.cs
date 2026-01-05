66612f3c56e4a88803ec0d61bb6491e6b68368fa
João Lima <jplima1853@gmail.com>
2026-01-05 07:31:14 -0300

    Interface para operações CRUD básicas

---
+++ PicpayChal.App/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using PicpayChal.App.Entities;
+
+namespace PicpayChal.App.Repositories.Interfaces;
+
+public interface IBaseRepository<T> where T : BaseEntity
+{
+    T Create(T entity);
+    T Update(T entity);
+    void Delete(T entity);
+
+    Task<T> GetById(long id);
+    Task<List<T>> GetAll();
+}
