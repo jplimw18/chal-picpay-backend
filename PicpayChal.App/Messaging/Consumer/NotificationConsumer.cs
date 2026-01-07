8bc703fb91ded86f2d7e05c27df2453750af8cdb
João Lima <jplima1853@gmail.com>
2026-01-07 11:38:52 -0300

    Consumer para lançar notificações para o serviço de notificação externo

---
+++ PicpayChal.App/Messaging/Consumer/NotificationConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using PicpayChal.App.Messaging.Contracts;
+using PicpayChal.App.Services.External;
+
+namespace PicpayChal.App.Messaging.Consumer;
+
+public class NotificationConsumer(INotificationApi notification)
+    : IConsumer<NotifyTransfer>
+{
+
+    private readonly INotificationApi _notification = notification;
+
+    public async Task Consume(ConsumeContext<NotifyTransfer> context)
+    {
+        var message = context.Message;
+
+        var notifyResponse = await _notification.Notify();
+        if (!notifyResponse.Message)
+            throw new NotificationException($"Falha ao enviar notificação para transferência: [{message.TransferId}]");
+    }
+}
