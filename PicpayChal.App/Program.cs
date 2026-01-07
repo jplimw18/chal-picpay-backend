74f4d1890771885e4bee98030b0abea21be315a8
João Lima <jplima1853@gmail.com>
2026-01-07 12:05:48 -0300

    Adicionando exception handler às configurações de inicialização

--- PicpayChal.App/Program.cs
+++ PicpayChal.App/Program.cs
@@ -1,10 +1,14 @@
 using PicpayChal.App;
+using PicpayChal.App.Exceptions.Handler;
 using PicpayChal.App.Services.External;
 using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
