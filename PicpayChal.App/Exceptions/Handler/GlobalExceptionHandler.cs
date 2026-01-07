a8f221e37f1a5db7a8293489cb42ce33edda8624
João Lima <jplima1853@gmail.com>
2026-01-07 12:03:56 -0300

    Criando tratamento de exceções globais

---
+++ PicpayChal.App/Exceptions/Handler/GlobalExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PicpayChal.App.Exceptions.Handler;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) 
+    : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger = logger;
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Ocorreu um erro inesperado: {Message}", exception.Message);
+
+        (int statusCode, string title) = exception switch
+        {
+            TransactionException => (StatusCodes.Status422UnprocessableEntity, "Falha na transação"),
+            NotificationException => (StatusCodes.Status503ServiceUnavailable, "Falha ao notificar transação"),
+            _ => (StatusCodes.Status500InternalServerError, "Erro interno do servidor")
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = title,
+            Detail = exception.Message,
+            Instance = httpContext.Request.Path,
+            Type = exception.GetType().Name            
+        };
+
+
+        httpContext.Response.StatusCode = statusCode;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
