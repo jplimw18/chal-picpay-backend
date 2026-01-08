d39d93fa985d7259772c37bc43794e22aa85d2c1
João Lima <jplima1853@gmail.com>
2026-01-07 22:09:03 -0300

    controller para receber chamadas dentro do contexto de transferências

---
+++ PicpayChal.App/Controllers/TransactionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PicpayChal.App.DTO;
+using PicpayChal.App.Services.Interfaces;
+
+namespace PicpayChal.App.Controllers;
+
+[ApiController]
+[Route("transfer")]
+public class TransactionController(
+    ITransactionService transactionService,
+    INotificationService notificationService
+)
+    : ControllerBase
+{
+    private readonly ITransactionService _transactionService = transactionService;
+    private readonly INotificationService _notificationService = notificationService;
+
+
+    [HttpPost]
+    public async Task<ActionResult> Transfer(TransactionRequest request)
+    {
+        var transactionId = await _transactionService.Transfer(request);
+        await _notificationService.Notify(transactionId);
+
+        return Created();
+    }
+}
